03233dea22d9603ef00c8776231d8698cb49754f
Sacredice <122034500+Sacredice@users.noreply.github.com>
2024-12-08 20:36:29 +0300

    navbar, footer

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -4,8 +4,8 @@ function NavigationBar() {
   return (
     <nav className='bg-blue-900 h-12 flex justify-center items-center'>
       <div className='w-full flex justify-between items-center mx-5 xl:mx-8'>
-        <p className='text-slate-100 text-xl'>Çılgın Motorcular</p>
-        <p className='text-slate-100 text-xl'>Çılgın Motorcular</p>
+        <p className='text-slate-100 text-xl'>Nav Bar</p>
+        <p className='text-slate-100 text-xl'>Menu</p>
       </div>
     </nav>
   )
