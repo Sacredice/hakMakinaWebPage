f5c729f93c65d15cb5c755de3b484132bb6fe749
Sacredice <anil_esen@hotmail.com>
2026-03-10 18:12:41 +0300

    non used imports coment out

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,10 +1,10 @@
-
+import { Link } from "react-router-dom";
 
 function NavigationBar() {
   return (
     <nav className='bg-blue-900 h-12 flex justify-center items-center'>
       <div className='w-full flex justify-between items-center mx-5 xl:mx-8'>
-        <p className='text-slate-100 text-xl'>Nav Bar</p>
+        <Link to="/" className='text-slate-100 text-xl'>Nav Bar</Link>
         <p className='text-slate-100 text-xl'>Menu</p>
       </div>
     </nav>
--- src/pages/ContactForm.tsx
+++ src/pages/ContactForm.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useForm } from "react-hook-form";
+// import { useState } from "react";
+// import { useForm } from "react-hook-form";
 
 function ContactForm() {
   return (
